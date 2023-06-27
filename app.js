4a0ba104c1b4c9b869b8a817641f7bbc6349cbb1
Tunahan Kilic <tunahankilic942@gmail.com>
2023-06-27 13:52:17 +0300

    Template engine (ejs) was applied to the project.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
 app.use(express.static('public'));
 
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+
+  app.get('/about', (req, res) => {
+    res.render('about');
+  });
+
+  app.get('/add', (req, res) => {
+    res.render('add');
+  });
 
 const port = 3000;
 app.listen(port, () => {
